463c86d16ade63e3d7ac6e2f765232a90accfa37
bily <githinjibcw@hotmail.com>
2020-05-11 19:29:28 -0400

    [update] save and load interactive 3d figures

--- Visualize/plotter_3d.py
+++ Visualize/plotter_3d.py
@@ -16,11 +16,7 @@ def plot_curve(bezier_curve, ctrl_points, filepath=""):
     X = bezier_curve[:, 0]
     Y = bezier_curve[:, 1]
     Z = bezier_curve[:, 2]
-    ax.plot(X, Y, Z)
-    #from matplotlib import cm
-    #cset = ax.contour(X, Y, Z, zdir='z', offset=np.min(Z), cmap=cm.coolwarm)
-    #cset = ax.contour(X, Y, Z, zdir='x', offset=np.min(X), cmap=cm.coolwarm)
-    #cset = ax.contour(X, Y, Z, zdir='y', offset=np.min(Y), cmap=cm.coolwarm)
+    ax.plot(X, Y, Z, markersize=5)
 
     # Plot control points
     ax.scatter(ctrl_points[:, 0], ctrl_points[:, 1], ctrl_points[:, 2], c="red")
@@ -34,13 +30,14 @@ def plot_curve(bezier_curve, ctrl_points, filepath=""):
     if filepath is "":
         plt.show()
     else:
+        #TODO: save inputs for later as in plot_clusters
         plt.savefig(filepath)
         pickle_file = open(filepath.split(".")[0] + ".pickle", 'wb')
         pickle.dump(plt.gcf(), pickle_file)
         pickle_file.close()
 
 
-def plot_clusters(clusters, centers, filepath=""):
+def plot_clusters(clusters, centers, filepath="", key_points=None):
     assert(clusters[1].shape[0] == 3), f"expected 3-d cluster points, got: {clusters[1].shape}"
     assert(centers.shape[0] == 3), f"expected 3-d centers, got: {centers.shape}"
 
@@ -53,21 +50,27 @@ def plot_clusters(clusters, centers, filepath=""):
             ax.scatter(clusters[idx][0, :],  # plot x
                         clusters[idx][1, :],  # plot y
                         clusters[idx][2, :],  # plot z
-                        cmap="coolwarm")
+                        cmap="coolwarm", s=5)
 
     # Plot centers
-    ax.scatter(centers[0, :], centers[1, :], centers[2, :], s=30, marker="x", label="centers")
+    ax.scatter(centers[0, :], centers[1, :], centers[2, :], s=30, marker="s", c="black")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.ylabel("z")
-    #plt.legend()
 
     if filepath is "":
         plt.show()
     else:
+        # Ordinary save for quick peek - not interactive
         plt.savefig(filepath)
+        # Save inputs to regenerate interactive figure later
         pickle_file = open(filepath.split(".")[0] + ".pickle", 'wb')
-        pickle.dump(plt.gcf(), pickle_file)
+        inputs = {
+           "clusters" : clusters,
+           "centers" : centers,
+           "key_points" : key_points,
+        }
+        pickle.dump(inputs, pickle_file)
         pickle_file.close()
 
     plt.clf()
--- Visualize/view_interactive_figure.py
+++ Visualize/view_interactive_figure.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 import pickle
 import sys
 
+from Visualize.plotter_3d import plot_clusters
+
 
 if __name__ == "__main__":
    if len(sys.argv) != 2:
       print("Usage:\n\t.py <fig_pickle_path>")
       sys.exit(1)
 
-   fig_path = sys.argv[1]
-   if ".pickle" not in fig_path.lower():
-      print(f"'{fig_path}' is not a .pickle file")
+   fig_data_path = sys.argv[1]
+   if ".pickle" not in fig_data_path.lower():
+      print(f"'{fig_data_path}' is not a .pickle file")
       sys.exit(1)
 
-   fig = pickle.load(open(fig_path, 'rb'))
+   fig_data = pickle.load(open(fig_data_path, 'rb'))
+   plot_clusters(fig_data["clusters"], fig_data["centers"], "", fig_data["key_points"])
+
    plt.show()
 
